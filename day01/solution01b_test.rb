b536a8af4700a509fd77e788b3fea4c64303f7d1
Steven Brown <sgbrown@mac.com>
2016-12-03 13:40:01 +1300

    setup for day 1, 2nd solution

---
+++ day01/solution01b_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require_relative 'solution01b'
+
+class AOC16Test < Minitest::Test
+
+  def test_example_1
+    expected = 4
+    assert_equal expected, ::AOC16.new.blocks("R8, R4, R4, R8")
+  end
+
+end
