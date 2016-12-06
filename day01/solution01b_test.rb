b8c2f70b965daa9424666899d215714b04526daa
Steven Brown <sgbrown@mac.com>
2016-12-06 19:18:49 +1300

    further setup

--- day01/solution01b_test.rb
+++ day01/solution01b_test.rb
@@ -6,7 +6,7 @@ class AOC16Test < Minitest::Test
 
   def test_example_1
     expected = 4
-    assert_equal expected, ::AOC16.new.blocks("R8, R4, R4, R8")
+    assert_equal expected, ::AOC16.new.blocks_first_repeat("R8, R4, R4, R8")
   end
 
 end
